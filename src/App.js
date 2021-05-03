883201df774b8c6c453335a12de03087e83887d8
오진주 <jinjuoh@hancom.com>
2021-05-03 15:10:09 +0900

    4단원 끝, 5단원 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import {Fragment} from "react";
 import './App.css';
+import Say from "./Say";
+import EventPractice from "./EventPractice";
+import EventPractice2 from "./EventPractice2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return <EventPractice2/>
 }
 
 export default App;
